082b354f90ad97eb121e839b3adf6cebbc1d985c
tim <tdixon51793@gmail.com>
2012-04-01 04:02:51 -0500

    lie about $SHELL

--- config.fish
+++ config.fish
@@ -1,5 +1,10 @@
 set PATH /usr/local/share/python ~/code/bin /usr/local/bin $PATH
 
+# vim and some other things rely on SHELL being a posixy sh this may cause
+# problems in the future, in which case I'll define a little function to wrap
+# vim and give it a fake environment variable.
+set SHELL (which sh)
+
 # source all of the files in the function directory
 for f in (find ~/.config/fish/functions -type f)
     . $f
