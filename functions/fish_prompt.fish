71fa699dca080329233f45768ed36b811029d051
tim <tdixon51793@gmail.com>
2012-04-01 13:12:22 -0500

    A nice way to tell the git status of the working directory.

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -14,8 +14,13 @@ function fish_prompt
     if test $current_venv
         with_color cyan -n "[$current_venv] "
     end
-
+    
     with_color $fish_color_cwd -n (prompt_pwd)
 
+    if is-git
+        set-git-color
+    end
+
     echo " » "
+    set_color normal
 end
