3e537e57208102b3b7eb737d9605ef8f3121740e
tim <tdixon51793@gmail.com>
2012-04-01 03:58:22 -0500

    moved `with_color` to functions/fish_prompt.fish.
    Sometimes i'd get an error because fish_prompt.fish was being sourced
    before with_color existed.

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,3 +1,15 @@
+function with_color
+    # set the color to the first argument
+    set_color $argv[1]
+    # delete that argument
+    set -e argv[1]
+    # and then pass the rest to echo, so we can do things like
+    # `with_color cyan -n hi`.
+    echo $argv
+    # and then go back to normal
+    set_color normal
+end
+
 function fish_prompt
     if test $current_venv
         with_color cyan -n "[$current_venv] "
