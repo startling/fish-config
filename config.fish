84fb4beeb933510deb1b0ae3e678facc7cbbe5a7
startling <tdixon51793@gmail.com>
2012-10-13 18:31:47 -0400

    more stuff in my PATH.

--- config.fish
+++ config.fish
@@ -1,5 +1,5 @@
-set PATH /usr/local/share/python ~/code/bin /usr/local/bin ~/.cabal/bin $PATH
-
+set PATH /usr/local/share/python ~/code/bin /usr/local/bin /usr/local/share/npm/bin ~/.cabal/bin $PATH
+set NODE_PATH /usr/local/share/npm/lib/node_modules/
 # vim and some other things rely on SHELL being a posixy sh this may cause
 # problems in the future, in which case I'll define a little function to wrap
 # vim and give it a fake environment variable.
